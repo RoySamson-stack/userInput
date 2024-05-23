b7597d254aefe98b1e35f8155330e553e5a9e484
RoySamson-stack <you@example.com>
2024-05-23 12:37:52 +0300

    PLP Week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name ? ")
+age = input("What is your age ?")
+location = input("What is you current location ?")
+print(f'Hello {name}, you are {age} years old and live in {location}')
