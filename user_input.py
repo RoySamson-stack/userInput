457f753752a71ae8ffafb36d0d9bd5705c50daf1
RoySamson-stack <you@example.com>
2024-05-23 13:12:13 +0300

    adding the plp week 2 python assignment

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name = input("What is your name ? ")
-age = input("What is your age ?")
-location = input("What is you current location ?")
-print(f'Hello {name}, you are {age} years old and live in {location}')
